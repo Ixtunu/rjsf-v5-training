79af56e083d90d0eeb67034e58c3388fa729b718
Ixtunu <firefox3991@gmail.com>
2025-02-28 21:25:53 +0900

    Install React JSON Schema Form v5

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import JSONSchemaForm from "./components/JSONSchemaForm";
+
+function App() {
+  return (
+    <div className="App">
+        <JSONSchemaForm />
+    </div>
+  );
+}
+
+export default App
